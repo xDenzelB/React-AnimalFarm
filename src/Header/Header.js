944f4c0b2f8a1a112bd998784408cfb8fb1e8985
Denzel <dbartolaba123@gmail.com>
2022-01-25 15:16:13 -0800

    Added my animals, data, and js files

---
+++ src/Header/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Header(props) {
+  return <header>Welcome to Alchemy Animal Farm: {props.name}</header>;
+}
---
+++ src/Main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import backgroundImg from '../background.png';
+import './Main.css';
+export default function Main() {
+  return (
+    <main style={{ backgroundImage: `url(${backgroundImg}` }}>
+          
+    </main>
+  );
+}

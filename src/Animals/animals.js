d85f4b70b014c35c1c93373d09ef16b9937a1d4b
Denzel <dbartolaba123@gmail.com>
2022-01-25 15:38:16 -0800

    fixed more on the animal.js file added map

--- src/Animals/animals.js
+++ src/Animals/animals.js
@@ -3,10 +3,17 @@ import React from 'react';
 export default function animals(props) {
   return (
     <div className='animal-card'>
-      <img src={`/${props.name}.png`} />
-      <p>{props.name}</p>
-      <p>Says: {props.says}</p>
-
+      {
+        props.animals.animals.map((item) => (
+          <div className='animal' key={item.name} style={{ left: `${item.left}`, top: `${item.top}` }}>
+        
+      
+            <img src={`/${props.type}.png`} />
+            <p>{props.name}</p>
+            <p>Says: {props.says}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
